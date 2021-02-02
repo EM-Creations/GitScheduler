849e2e3e17d5314d872f3d6101075c766cd816f9
Edward McKnight <eddy@em-creations.co.uk>
2021-02-02 18:59:01 +0000

    New: Attempt to add Gradle.

--- app/src/ui/Controller.java
+++ app/src/ui/Controller.java
@@ -5,12 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 
 public class Controller {
+    private static Logger logger = LogManager.getLogger(Controller.class);
 
     @FXML
     private MenuItem exitButton;
 
     @FXML
     void exit(ActionEvent event) {
+        logger.info("Closing GitScheduler..");
+
         System.exit(0);
     }
 
--- app/src/ui/Main.java
+++ app/src/ui/Main.java
@@ -7,9 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static Logger logger = LogManager.getLogger(Main.class);
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        logger.info("Starting GitScheduler..");
+
         String homeSceneResource = "home.fxml";
         String title = "GitScheduler";
         int sceneHeight = 500, sceneWidth = 500;
